6bf4f0fb0e4fd3f0785f2e53b41e00b5ffb928ac
Drew Anderson <drw.a@outlook.com>
2017-09-27 10:32:11 -0400

    initial

---
+++ index.js
@@ -0,0 +1,49 @@
+const validator = require('validator')
+
+let filters = {
+    phone: [
+        (s) => {      
+            return s.replace(/\D/g, '') // remove all non-digits
+        },
+        (s) => {
+            return s.length === 11 ? s.replace(/^1/,'') : s
+        },
+        (s) => {
+            return (s.length === 10) ? s : 0
+        }
+        // standard formatting here
+    ],
+    name: [
+        (s) => {
+            return s.replace(/[^a-z\s\-\.]/gi, '') // remove everything except letters, spaces, hyphen, period
+        }
+    ],
+    alpha: [
+        (s) => {
+            return s.replace(/[^a-z]/gi, '')
+        }
+    ],
+    email: [
+        (s) => {
+            return (validator.isEmail(s)) ? s : 0
+        }
+    ],
+    cc: [
+        (s) => {
+            return (validator.isCreditCard(s)) ? s : 0
+        }
+    ],
+    zip: [
+        (s) => {
+            return (validator.isPostalCode(s,'US')) ? s : 0
+        }
+    ],
+    stateLetters: [
+        (s) => {
+            return (/^(?:(A[KLRZ]|C[AOT]|D[CE]|FL|GA|HI|I[ADLN]|K[SY]|LA|M[ADEINOST]|N[CDEHJMVY]|O[HKR]|P[AR]|RI|S[CD]|T[NX]|UT|V[AIT]|W[AIVY]))$/.test(s)) ?
+                s : 0
+        }
+    ]
+}
+
+module.exports = filters
